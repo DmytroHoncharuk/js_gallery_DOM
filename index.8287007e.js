ea21a4d53dcca59cb8c1306021153b4ecd189c73
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-10-30 09:48:46 +0200

    make build

---
+++ index.8287007e.js
@@ -0,0 +1,2 @@
+for(var e=document.getElementById("largeImg"),t=document.getElementsByTagName("a"),a=0;a<t.length;a++)t[a].addEventListener("click",function(t){if("IMG"===t.target.tagName){t.preventDefault();var a=t.target.parentElement;e.src=a.href}});
+//# sourceMappingURL=index.8287007e.js.map
